1ee9cece03bf96f242877a213dc4afc2a1cc18fc
MinalSalvi <salviminal21@gmail.com>
2022-12-09 23:28:26 +0530

    initial change

---
+++ src/app/r-and-s/r-and-s.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-r-and-s',
+  templateUrl: './r-and-s.component.html',
+  styleUrls: ['./r-and-s.component.css']
+})
+export class RAndSComponent {
+
+}
